2fabeaa98124953bcdeb868aa3a94ff51c9cda56
= <118738889+RifkiSstwnn@users.noreply.github.com>
2024-03-14 13:30:02 +0700

    fillable

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -12,27 +12,26 @@ class UserController extends Controller
       // return view('user',['data'=>$user]);
 
       // tambah data user dengan Eloquent Model
-        // $data = [
-        //   'username' => 'customer-1',
-        //   'nama' => 'Pelanggan',
-        //   'password' => Hash::make('12345'),
-        //   'level_id' => 4
-        // ];
-        // UserModel::insert($data); // tambahkan data ke tabel m_user
+        $data = [
+          'username' => 'Manager 3',
+          'nama' => 'Manager_tiga',
+          'password' => Hash::make('12345'),
+          'level_id' => 2
+        ];
+        UserModel::create($data); // tambahkan data ke tabel m_user
 
-        // // coba akses model UserModel
-        // $user = UserModel::all(); // ambil semua data dari tabel m_user
-        // return view('user', ['data' => $user]);
-
+        // coba akses model UserModel
+        $user = UserModel::all(); // ambil semua data dari tabel m_user
+        return view('user', ['data' => $user]);
 
         //tambah data user dengan Eloquent Model
-        $data = [
-           'nama' => 'Pelanggan Pertama',
-        ];
-        UserModel::where('username', 'customer-1')->update($data); //update data
+        // $data = [
+        //    'nama' => 'Pelanggan Pertama',
+        // ];
+        // UserModel::where('username', 'customer-1')->update($data); //update data
         
-        //coba akses model UserModel
-        $user = UserModel::all(); //ambil semua data dari tabel m_user
-        return view('user', ['data' =>$user]);
+        // //coba akses model UserModel
+        // $user = UserModel::all(); //ambil semua data dari tabel m_user
+        // return view('user', ['data' =>$user]);
     }
 }
