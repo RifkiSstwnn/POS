584f972b4121188e9e96a8a8d7ae8a1bd7c7ae2d
= <118738889+RifkiSstwnn@users.noreply.github.com>
2024-03-14 14:23:04 +0700

    Not found Exception

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -12,17 +12,17 @@ class UserController extends Controller
       // return view('user',['data'=>$user]);
 
       // tambah data user dengan Eloquent Model
-        $data = [
-          'username' => 'Manager 3',
-          'nama' => 'Manager_tiga',
-          'password' => Hash::make('12345'),
-          'level_id' => 2
-        ];
-        UserModel::create($data); // tambahkan data ke tabel m_user
+        // $data = [
+        //   'username' => 'Manager 3',
+        //   'nama' => 'Manager_tiga',
+        //   'password' => Hash::make('12345'),
+        //   'level_id' => 2
+        // ];
+        // UserModel::create($data); // tambahkan data ke tabel m_user
 
         // coba akses model UserModel
-        $user = UserModel::all(); // ambil semua data dari tabel m_user
-        return view('user', ['data' => $user]);
+        // $user = UserModel::firstWhere('level_id',1); // ambil semua data dari tabel m_user
+        // return view('user', ['data' => $user]);
 
         //tambah data user dengan Eloquent Model
         // $data = [
@@ -33,5 +33,16 @@ class UserController extends Controller
         // //coba akses model UserModel
         // $user = UserModel::all(); //ambil semua data dari tabel m_user
         // return view('user', ['data' =>$user]);
+
+        // $user = UserModel::findOr(20,['username', 'nama'], function (){
+        //   abort(404);
+        // });
+        // return view('user', ['data' => $user]);
+
+        // $user = UserModel::findOrFail(465);
+        // return view('user', ['data' => $user]);
+
+         $user= UserModel::where('username', 'Manager9')->firstOrFail();
+         return view('user', ['data' => $user]);
     }
 }
